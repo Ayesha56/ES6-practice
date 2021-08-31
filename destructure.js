50d58cadfb35165cf570a6276ff61f206071dcf7
Ayesha56 <ayesha56sid@gmail.com>
2021-08-31 15:47:44 +0600

    destructureJS

---
+++ destructure.js
@@ -0,0 +1,17 @@
+const product = { name: "Laptop", model: "Yoga 3", price: 49000, dusk: "512 SSD" };
+const { price } = product;
+console.log(price);
+
+const person = { name: 'Jack William', age: 17, job: 'facebooker', gfName: 'Ema Watson', address: 'Kochu Khet', phone: '01717112211', friends: ['Tom hancks', 'tom cruise', 'Tom yarn'] }
+
+const { address, phone, gfName, salary } = person;
+// const gfName = person.gfName;
+// const phone = person.phone;
+
+// console.log(gfName, phone, salary, address);
+// console.log(gfName, phone, salary, address);
+
+
+const friends = ['Sakib Khan', 'Arman Khan', 'Aamir Khan', 'Salman Khan', 'sharukh khan'];
+const [chotoFriend, ...olderF] = friends;
+console.log(olderF);
