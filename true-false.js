230188e4216d1f253b8cd061051e0c178684e982
Ayesha56 <ayesha56sid@gmail.com>
2021-08-31 17:41:02 +0600

    false-true

---
+++ true-false.js
@@ -0,0 +1,14 @@
+const age = 4; // 0   bade sob true
+const name = "ayesha" //true
+const name1 = "" //false hobe
+    //const name2; //set na takle undified hole false hobe +""+0+null+NAN//
+    //----false----==> *undifend *null *0 * NAN *"" * false set korle
+    //true---------==> *'0',' ',[],
+const name2 = null;
+
+console.log(name2);
+if (name1.length) {
+    console.log("its true");
+} else {
+    console.log("its false");
+}
